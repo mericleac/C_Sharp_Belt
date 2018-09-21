a5ae688fc5a88a14e625744729d3e99884276f37
Mandy Mericle <33289865+mericleac@users.noreply.github.com>
2018-09-21 13:52:18 -0500

    Deployment Setup

--- Program.cs
+++ Program.cs
@@ -19,7 +19,10 @@ namespace BeltTemplate
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
+                .UseIISIntegration()
                 .Build();
     }
 }
